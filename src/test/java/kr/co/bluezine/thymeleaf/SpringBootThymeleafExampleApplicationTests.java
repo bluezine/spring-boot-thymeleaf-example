60259b937bbbc2699c12ad07025407d87301b70b
Kisig Ian Seo <bluezine0930@gmail.com>
2021-08-18 14:50:11 +0900

    Spring boot initialize

---
+++ src/test/java/kr/co/bluezine/thymeleaf/SpringBootThymeleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bluezine.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
